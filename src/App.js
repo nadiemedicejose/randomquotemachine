e951c718f8f6377515fa5ea10206691e719beea9
Jose Estrada <j.estrada49@icloud.com>
2021-06-04 22:21:09 -0700

    style and structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './styles/App.css';
 
+import { FaTwitter } from 'react-icons/fa';
+
 export default function App() {
   return (
-    <div>
-      
+    <div id='quote-box'>
+      <div id='text-container'>
+        <p id='text'>Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.</p>
+      </div>
+      <p id='author'>Martin Luther King Jr.</p>
+      <div id='buttons-container'>
+        <a id='new-quote' href='/'>New quote</a>
+        <a id='tweet-quote'
+          href='https://twitter.com/intent/tweet' target='_blank'
+          rel='noreferrer'><FaTwitter /> Tweet</a>
+      </div>
     </div>
   )
 }
